32d24d5a51bdbd7ecb8220c79099c660e1be03f4
Ariful0007 <arifulpub143@gmail.com>
2023-05-11 14:45:47 +0600

    ggggg

--- ImageProjects/ViewController.swift
+++ ImageProjects/ViewController.swift
@@ -12,6 +12,7 @@ import Photos
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
 
+    @IBOutlet weak var lll: UILabel!
     @IBOutlet var pickImage: UIView!
     @IBOutlet weak var imageView: UIImageView!
     var imagePickercontroller  = UIImagePickerController();
@@ -19,6 +20,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavi
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        lll.isHidden = true
     }
 
     @IBAction func selectImage(_ sender: UIButton) {
@@ -35,54 +37,79 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavi
     }
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let pickedImage = info[.originalImage] as? UIImage {
-               imageView.contentMode = .scaleAspectFill
-               imageView.image = pickedImage
-            let imageUri = info[.imageURL] as? URL
-            
-         
-            
-            
-        
-           
-           
+            imageView.contentMode = .scaleAspectFill
+            imageView.image = pickedImage
+            let imageUri = info[.imageURL] as?URL
+            print("Image url : \n")
+            print(imageUri)
             let stringValue = String(describing: imageUri)
-            print(stringValue)
-        
             let urlString = URL(string: stringValue)
             let name = urlString?.lastPathComponent
             print("image \n\n\n")
-            print(name)
-         
-            let imageData: Data = pickedImage.jpegData(compressionQuality: 0.4) ?? Data()
-            let encodedImage: String = imageData.base64EncodedString()
-         // print("Encoding Uri : \n\n\n")
-           //print(encodedImage)
-            let imagePath = String(describing: imageUri)
-            
-            
-
-           // print(imagePath)
-            
-          //  print(pickedImage)
-            
-            let imagegPath = "/path/to/your/image.jpg"
-
-            if let image = UIImage(contentsOfFile: imagePath) {
-                // Use the image object here
-                // For example, you can display it in an image view:
-                let imageView = UIImageView(image: image)
-                
-                // Add the imageView to your view hierarchy
-            } else {
-                // Failed to load the image
-                print("Failed to load image from file path: \(imagePath)")
-            }
+            var mainstring = name
+            let sub:Character = ")"
+            mainstring = mainstring?.replacingOccurrences(of: String(sub), with: "")
+            print(mainstring)
+            lll.text = mainstring
+            lll.isHidden = false
            }
            //self.dismiss(animated: true)
         self.dismiss(animated: true)
         
         
     }
+    /*
+     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+         if let pickedImage = info[.originalImage] as? UIImage {
+                imageView.contentMode = .scaleAspectFill
+                imageView.image = pickedImage
+             let imageUri = info[.imageURL] as? URL
+             
+          
+             
+             
+         
+            
+            
+             let stringValue = String(describing: imageUri)
+             print(stringValue)
+         
+             let urlString = URL(string: stringValue)
+             let name = urlString?.lastPathComponent
+             print("image \n\n\n")
+             print(name)
+          
+             let imageData: Data = pickedImage.jpegData(compressionQuality: 0.4) ?? Data()
+             let encodedImage: String = imageData.base64EncodedString()
+          // print("Encoding Uri : \n\n\n")
+            //print(encodedImage)
+             let imagePath = String(describing: imageUri)
+             
+             
+
+            // print(imagePath)
+             
+           //  print(pickedImage)
+             
+             let imagegPath = "/path/to/your/image.jpg"
+
+             if let image = UIImage(contentsOfFile: imagePath) {
+                 // Use the image object here
+                 // For example, you can display it in an image view:
+                 let imageView = UIImageView(image: image)
+                 
+                 // Add the imageView to your view hierarchy
+             } else {
+                 // Failed to load the image
+                 print("Failed to load image from file path: \(imagePath)")
+             }
+            }
+            //self.dismiss(animated: true)
+         self.dismiss(animated: true)
+         
+         
+     }
+     */
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         print("Image picked declined")
     }
